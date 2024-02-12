0e36dab6a111a4bad6b75568d54cd5a0c344ab67
nkrishnan <nkrishnan3100@gmail.com>
2024-02-12 16:32:20 +0530

    db migration 1st commit

---
+++ src/main/java/com/scaler/lldprojectmodule/dtos/Role.java
@@ -0,0 +1,13 @@
+package com.scaler.lldprojectmodule.dtos;
+
+
+import com.scaler.lldprojectmodule.models.BaseModel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Role extends BaseModel {
+    private String name;
+
+}
